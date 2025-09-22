8c72bd7df625213454b8d26371d38914374c5366
Jon Bågling <bagling@gmail.com>
2025-09-22 11:07:27 +0200

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-10">
+      <h1 className="text-4xl sm:text-6xl font-extrabold text-center sm:text-left">
+        Trade Logs
+      </h1>
+    </main>
+  );
+}
