0f9b1e84947e6c4e7fb171ecf1a3abc3c1c96b74
Jon Bågling <bagling@gmail.com>
2025-12-11 12:15:15 +0100

    add skeleton pulse animation

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,12 +1,8 @@
 // Loading animation
-const shimmer =
-  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
 
 export function CardSkeleton() {
   return (
-    <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
-    >
+    <div className="animate-pulse relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm">
       <div className="flex p-4">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
         <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
@@ -31,9 +27,7 @@ export function CardsSkeleton() {
 
 export function ActiveTradesSkeleton() {
   return (
-    <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-4 shadow-sm flex w-full flex-col md:col-span-4`}
-    >
+    <div className="animate-pulse relative overflow-hidden rounded-xl bg-gray-100 p-4 shadow-sm flex w-full flex-col md:col-span-4">
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4"></div>
     </div>
   );
@@ -41,9 +35,7 @@ export function ActiveTradesSkeleton() {
 
 export function TradeListSkeleton() {
   return (
-    <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm mt-6 flow-root`}
-    >
+    <div className="animate-pulse relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <table className="hidden min-w-full text-gray-900 md:table">
